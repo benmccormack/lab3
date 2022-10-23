a6bb34914870609d822c46ae60d011281ec5c27b
benmccormack <c19459424@mytudublin.ie>
2022-10-23 17:10:12 +0100

    Contact number filter and error message have been added

--- problem-1/app.js
+++ problem-1/app.js
@@ -21,3 +21,34 @@ function addContact(){
     cell3.innerHTML = email;
 }
 
+function filterContacts(){
+    var input = document.getElementById("search");
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("contacts");
+    var row = document.getElementsByTagName("tr");
+    var error = document.getElementById("noResult");
+
+    //looping through each row and hiding the rows that do not match
+    for(i=0; i<row.length; i++){
+        // getting the elements from the phone number coplumn of each row.
+        td = row[i].getElementsByTagName("td")[1];
+        if(td){
+            var text = td.textContent || td.innerText;
+            if(text.toUpperCase().indexOf(filter) >- 1){
+                row[i].style.display = "";
+                error.style.display="none";
+            } else {
+                row[i].style.display="none";
+                error.style.display="block";
+            }
+        }
+    }
+}
+
+function sortNames(){
+    var table = document.getElementById("contacts");
+    var rows = table.rows;
+
+    //compare each element to the next until completed - what sorting algorithm is this?!
+    
+}
