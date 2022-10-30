e7f54f0996cb9a3905fa32740f7043b07de4dda7
benmccormack <c19459424@mytudublin.ie>
2022-10-30 12:04:44 +0000

    Half of part 2 completed

--- problem-2/app.js
+++ problem-2/app.js
@@ -1,6 +1,16 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  //.then(json => console.log(json))
-  .then(json => Array.from(json))
- 
-  //log titles with more than 6 words to the console - think splitting by "" and counting splits? If more than 5 splits then there are 6 or more words.
+.then(response => response.json())
+.then(posts => {
+  console.log("Titles with more than 6 words")
+  let wordResult = posts.filter(posts => titleCount(posts.title))
+  console.log(wordResult);
+
+});
+
+// Title word count to show the titles that are more than 6
+function titleCount(title) {
+  const posts = title.split(' ');
+  return posts.filter(word => word !== '').length > 6;
+}
+
+// map the fequecy of words - maybe try to return in the format
